875044def65ce9e6e7f8a4f022772788a584e3c2
Olga Kaplunivska <kaplunovskaya.o@gmail.com>
2023-03-23 16:27:59 +0100

    add change

--- src/components/UIcomponents/Languages/LanguagesTheme.js
+++ src/components/UIcomponents/Languages/LanguagesTheme.js
@@ -1,11 +1,11 @@
 export const theme = {  
   menuItem: {
-    fontSize: '1.4rem',
+    fontSize: '1.3rem',
     color: 'secondary.main',    
     backgroundColor: 'primary.main',
   },
   selectItem: {
-    fontSize: '1.4rem',
+    fontSize: '1.3rem',
     color: 'text.primary',
   },
 };
