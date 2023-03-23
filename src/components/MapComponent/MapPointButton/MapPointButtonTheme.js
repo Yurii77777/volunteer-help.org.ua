2f14a1bf35ecc04274d1ade340c7eda66c21defe
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-03-23 11:08:26 +0200

    add map point button component

---
+++ src/components/MapComponent/MapPointButton/MapPointButtonTheme.js
@@ -0,0 +1,24 @@
+export const theme = {
+  box: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    width: '20%',
+
+    border: '1px solid #dee2e6',
+    borderRadius: '5px',
+    padding: '10px 5px',
+    marginBottom: '5px',
+
+    cursor: 'pointer',
+  },
+  icon: { marginRight: '7px' },
+  boxText: {
+    textAlign: 'center',
+    wordBreak: 'break-word',
+  },
+  text: {
+    fontSize: '0.8rem',
+    marginBottom: '2px',
+  },
+};
